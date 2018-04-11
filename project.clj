68d13aa1ec6099d2a24c96254cc4f09905e1c0d7
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-04-11 12:03:54 +0800

    Add uberjar support

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :min-lein-version  "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^skip-aot daikin.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
   :source-paths      ["src/main/clojure"]
   :java-source-paths ["src/main/java"])
 
